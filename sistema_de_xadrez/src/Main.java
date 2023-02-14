c02108f48733a8404186c8487229b15c5ec670bb
IgorHSN <pedroigor@gmail.com>
2023-02-14 11:05:17 -0300

    implementando movimentos do rei

--- sistema_de_xadrez/src/Main.java
+++ sistema_de_xadrez/src/Main.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
 
 public class Main {
 	public static void main(String[] args) {
